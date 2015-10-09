400c78d610a98469c28ecab8aadc3ca727b1266d
Dulmandakh <dulmandakh@gmail.com>
2015-10-09 11:17:05 +0800

    Update restapi.js
    beforeOpen option that can be used to set xhr properties before open is called. For example, we can use it to set password and username on HTTPClient instance.

--- restapi.js
+++ restapi.js
@@ -84,6 +84,10 @@ function apiCall(_options, _callback) {
 			cleanup();
 		};
 		
+		if (_options.beforeOpen) {
+			_options.beforeOpen(xhr);
+		}
+		
 		//Prepare the request
 		xhr.open(_options.type, _options.url);
 
